11202263130225217f04755947a7c58df1b2d7c5
lyy <wb-lyy569420@alibaba-inc.com>
2021-03-29 15:43:48 +0800

    feat: imgcook样式可用度 80%左右

--- src/App.js
+++ src/App.js
@@ -3,21 +3,91 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="block">
+        <img
+          className="longBanner"
+          src={
+            'https://img.yycdn.com/imgextra/i4/O1CN013u0XLV1sUFZYQDGBa_!!6000000005769-2-tps-750-1426.png'
+          }
+        />
+      </div>
+      <div className="hd">
+        <div className="bgWrap">
+          <img
+            className="bg"
+            src={
+              'https://img.yycdn.com/imgextra/i1/O1CN01kQo2PO1TPKvbHYYMZ_!!6000000002374-2-tps-380-59.png'
+            }
+          />
+        </div>
+        <div className="block_1">
+          <div className="container">
+            <img
+              className="actionBg"
+              src={
+                'https://img.yycdn.com/imgextra/i3/O1CN01N6VU2x1wLKJowLOEV_!!6000000006291-2-tps-215-64.png'
+              }
+            />
+            <img
+              className="buttonBg"
+              src={
+                'https://img.yycdn.com/imgextra/i1/O1CN01uWyP7U1T5BV3XNnO2_!!6000000002330-2-tps-171-60.png'
+              }
+            />
+            <div className="outer">
+              <div className="empty" />
+              <div className="wrap">
+                <div className="color" />
+              </div>
+              <div className="block_2">
+                <div className="colorDiv" />
+              </div>
+              <div className="group">
+                <div className="color_1" />
+              </div>
+            </div>
+          </div>
+          {true && (
+            <img
+              className="pic"
+              src={
+                'https://img.yycdn.com/imgextra/i3/O1CN01unxf2I1ByubtN0qJt_!!6000000000015-2-tps-110-110.png'
+              }
+            />
+          )}
+        </div>
+      </div>
+      <div className="bd">
+        <div className="group_1">
+          <img
+            className="icon"
+            src={
+              'https://img.yycdn.com/imgextra/i2/O1CN01KkuONB1TI0alMc6kd_!!6000000002358-2-tps-19-27.png'
+            }
+          />
+          <img
+            className="img"
+            src={
+              'https://img.yycdn.com/imgextra/i1/O1CN01f4nUpj1g9O8l55Kwj_!!6000000004099-2-tps-82-77.png'
+            }
+          />
+        </div>
+        <img
+          className="actionBg_1"
+          src={
+            'https://img.yycdn.com/imgextra/i4/O1CN01pZ3kdi1qWQDUZgPCY_!!6000000005503-2-tps-270-78.png'
+          }
+        />
+      </div>
+      <div className="ft">
+        <img
+          className="largeIcon"
+          src={
+            'https://img.yycdn.com/imgextra/i1/O1CN01DM6rqr1HiJxMHwA3o_!!6000000000791-2-tps-52-57.png'
+          }
+        />
+      </div>
     </div>
   );
 }
